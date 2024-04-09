cf35788cb51bab50fb78519c8186fb589dc23dfb
eruzich <eruzich@users.noreply.github.com>
2024-04-08 19:17:01 -0600

    Add files via upload

---
+++ GameOfCheckers.java
@@ -0,0 +1,29 @@
+package gp;
+
+public class GameOfCheckers {
+
+	private Player winner;
+	private boolean gameIsOver;
+	private CheckerBoard checkerBoard;
+	private Player redPlayer;
+	private Player whitePlayer;
+	
+	/**
+	* Constructs a new Game of Checkers.
+	*
+	* 	*/
+	public GameOfCheckers() {
+	}
+	
+	public void playGame() {
+		
+	}
+	public StdDraw drawPlayArea() {
+		return StdDraw;
+	}
+	
+	private StdDraw drawTakenCheckers() {
+		return StdDraw;
+	}
+	
+}
