e00e843433cb195aeee5cb87aedf6976ac581062
ErinsCode <74279783+ErinsCode@users.noreply.github.com>
2024-04-12 12:04:32 -0600

    Update Checker.java
    Added doc comments

--- Checker.java
+++ Checker.java
@@ -29,33 +29,76 @@ public class Checker {
 		isSelected = false;
 	}
 	
+	/**
+	 * Makes it so checker isSelected is true
+	 */
 	public void selectChecker()
 	{
 		isSelected = true;	
 	}
 	
-	public boolean getIsKing()
+	/**
+	 * Returns true if the checker is selected
+	 * @return if the checker is selected
+	 */
+	public boolean checkerIsSelected()
 	{
-		return isKing;
+		return isSelected;
 	}
 	
+	/**
+	 * Makes a checker a king
+	 */
 	public void kingMe()
 	{
 		isKing = true;
 	}
 	
+	/**
+	 * Return if checker is a king
+	 * @return Returns true if checker is a king
+	 */
+	public boolean getIsKing()
+	{
+		return isKing;
+	}
+	
+	/**
+	 * Returns the Digraph for the checker indicating all of the possible
+	 * moves it can currently take
+	 * @return returns digraph of possible moves
+	 */
 	public Digraph getPossibleMoves()
 	{
 		return possibleMoves;
 	}
 	
+	/**
+	 * Add an edge to a checker's directed graph of possible moves
+	 * @param start vertex to start at
+	 * @param end edge we are adding
+	 */
 	public void setPossibleMoves(int start, int end)
 	{
 		possibleMoves.addEdge(start, end);
 	}
 
+	/**
+	 * Resets the checker's directed graph so it is empty
+	 */
+	public void resetPossibleMoves()
+	{
+		possibleMoves = null;
+		possibleMoves = new Digraph(64);
+	}
+	/**
+	 * Get the checker's color
+	 * @return Returns color of checker
+	 */
 	public Color getColor()
 	{
 		return color;
 	}
+	
+	
 }
